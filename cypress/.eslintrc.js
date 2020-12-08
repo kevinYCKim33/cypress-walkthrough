a7ba7c99fb59309a18885a83330344806ad1fcb7
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-12-08 00:25:33 -0500

    cypress first lesson done

--- cypress/.eslintrc.js
+++ cypress/.eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-  root: true,
+  root: true, // to stuff
   plugins: ['eslint-plugin-cypress'],
   extends: ['kentcdodds', 'kentcdodds/import', 'plugin:cypress/recommended'],
-  env: {'cypress/globals': true},
+  env: {'cypress/globals': true}, // comes from ES Lint plugin we just installed
 }
+
+// some conflicts between
