674ee0fd7df8bfe228e47d717459f20d7c0dc852
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-12-08 23:18:06 -0500

    finish 9 of 20 test user reg with Cypress

--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -2,7 +2,7 @@ import {buildUser} from '../support/generate'
 
 describe('registration', () => {
   it('should register a new user', () => {
-    const user = buildUser()
+    const user = buildUser() //truly random user every time
     cy.visit('/')
       .findByText(/register/i)
       .click()
@@ -12,10 +12,11 @@ describe('registration', () => {
       .type(user.password)
       .findByText(/submit/i)
       .click()
-      .url()
-      .should('eq', `${Cypress.config().baseUrl}/`)
-      .window()
-      .its('localStorage.token')
-      .should('be.a', 'string')
+      .url() // we're now talking about the url...not the submit button or whatever
+      .should('eq', `${Cypress.config().baseUrl}/`) // `http://localhost:8080`
+      .window() // change subject to window
+      .its('localStorage.token') // check out its local storage value
+      .should('be.a', 'string') // this is good enough...
+    // kind of a fire test actually
   })
 })
