802472d9187dd5e67870ef26b961a41c38f97df5
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-12-20 23:49:33 -0500

    finish 17 of 20 Use cy.request to not overtest login logic

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -14,18 +14,37 @@ describe('anonymous calculator', () => {
   })
 })
 
+// cy.request(): actually make the call right then and there
+// cy.intercept() or cy.route() (now deprecated): intercept is better convention TBH
+
 describe('authenticated calculator', () => {
   it('displays the username', () => {
+    // Motivation: you already tested logging in in login.js, so no need to go through the whole
+    // logging in user actions
     cy.createUser().then(user => {
       cy.request({
         url: 'http://localhost:3000/login',
         method: 'POST',
         body: user,
       }).then(response => {
+        // a bit curious that I have to manually put in the localStorage action
+        // like I feel like I'm messing around with the internals
+        // or testing implementations
         window.localStorage.setItem('token', response.body.user.token)
       })
 
+      // magic flute your way to the logged in state of the app
+
       cy.visit('/')
+        // overtesting, this already gets tested in login.js
+        // .findByText(/login/i)
+        // .click()
+        // .findByLabelText(/username/i)
+        // .type(user.username)
+        // .findByLabelText(/password/i)
+        // .type(user.password)
+        // .findByText(/submit/i)
+        // .click()
         .findByTestId('username-display')
         .should('have.text', user.username)
         .findByText(/logout/i)
