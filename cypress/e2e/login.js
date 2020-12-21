dfdf3ba2356d5e7374a44eec126f3590cad6f0f2
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-12-20 20:42:23 -0500

    finish 14 of 20 Cypress.Commands.add

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -1,23 +1,27 @@
 describe('login', () => {
   it('should login an existing user', () => {
-    cy.createUser().then(user => {
-      cy.visit('/')
-        .findByText(/login/i)
-        .click()
-        .findByLabelText(/username/i)
-        .type(user.username)
-        .findByLabelText(/password/i)
-        .type(user.password)
-        .findByText(/submit/i)
-        .click()
-        // now let's verify things are set after login.
-        .url()
-        .should('eq', `${Cypress.config().baseUrl}/`)
-        .window()
-        .its('localStorage.token')
-        .should('be.a', 'string')
-        .findByTestId('username-display')
-        .should('have.text', user.username)
-    })
+    cy.createUser()
+      // the then here is very clutch
+      // the createUser adds a super response back, see commands.js
+      // it is a bit more confusing than just writing it the dumb way
+      .then(user => {
+        cy.visit('/')
+          .findByText(/login/i)
+          .click()
+          .findByLabelText(/username/i)
+          .type(user.username)
+          .findByLabelText(/password/i)
+          .type(user.password)
+          .findByText(/submit/i)
+          .click()
+          // now let's verify things are set after login.
+          .url()
+          .should('eq', `${Cypress.config().baseUrl}/`)
+          .window()
+          .its('localStorage.token')
+          .should('be.a', 'string')
+          .findByTestId('username-display')
+          .should('have.text', user.username)
+      })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,20 @@
 import {buildUser} from '../support/generate'
 
+// not so much voodoo
+// overrides ??
 Cypress.Commands.add('createUser', overrides => {
-  const user = buildUser(overrides)
+  const user = buildUser(overrides) // in case you want to feed in some add'l argument
+  // this doesn't do that good of an example
+  // just arguments is fine
   cy.request({
     url: 'http://localhost:3000/register',
     method: 'POST',
     body: user,
-  }).then(response => ({...response.body.user, ...user}))
+  })
+    // this is the low key clutch line
+    // why add in user here?
+    // we have access to the user's password here then
+    // we can then use it to type user.password
+    // cause you aren't just gonna get back the user's password from regular JSON
+    .then(response => ({...response.body.user, ...user}))
 })
